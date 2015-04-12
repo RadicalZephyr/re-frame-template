62a8b41fbadef3fcd27bdb463677c37a2ac2b667
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-12 13:33:19 -0500

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject zephyr-re-frame/lein-template "0.2.0-SNAPSHOT"
+(defproject zephyr-re-frame/lein-template "0.2.0"
   :description "My preferred way to setup a Clojure web app."
   :url "https://github.com/RadicalZephyr/re-frame-template"
   :license {:name "Eclipse Public License"
